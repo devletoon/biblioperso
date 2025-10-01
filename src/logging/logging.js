4d761cf3840a4191fc27a19c3f41951037b33596
Ulrich Tanguy <u.tanguykonan@gmail.com>
2025-10-01 21:33:57 +0000

    refonte de la structure

--- src/logging/logging.js
+++
@@ -1,9 +0,0 @@
-function connect() {
-    console.log("Connexion réussie !");
-}
-function signup() {
-    console.log("Imdisponible pour le moment !");
-}
-function forget() {
-    console.log("Imdisponible pour le moment !");
-}
