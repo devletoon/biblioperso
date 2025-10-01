051dcbdf80a704ae99a7c8abb19ce0d0961cea45
Ulrich Tanguy <u.tanguykonan@gmail.com>
2025-10-01 20:21:03 +0000

    creation du repo

---
+++ src/logging/logging.js
@@ -0,0 +1,9 @@
+function connect() {
+    console.log("Connexion réussie !");
+}
+function signup() {
+    console.log("Imdisponible pour le moment !");
+}
+function forget() {
+    console.log("Imdisponible pour le moment !");
+}
